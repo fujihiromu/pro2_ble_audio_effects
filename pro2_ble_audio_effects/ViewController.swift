af890b3527e618b172560a0dbaf403b63ceaa867
HiromuFuji <fuji.hiromu@gmail.com>
2017-10-28 13:57:26 +0900

    Initial Commit

---
+++ pro2_ble_audio_effects/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pro2_ble_audio_effects
+//
+//  Created by Fuji Hiromu on 2017/10/28.
+//  Copyright © 2017 藤　大夢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
